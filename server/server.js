8203686185cc412876131912976381cc76f789c0
aya-asylbek <aichurek0990@gmail.com>
2025-02-27 11:49:06 -0800

    Add weather API endpoint Sunnyvale city

--- server/server.js
+++ server/server.js
@@ -12,8 +12,9 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>console.log(`Server started on port ${PORT}`))
 
 
-app.get('/weather', async (req, res) => {
-    const city = req.query.city
+app.get('/weather/:city', async (req, res) => {
+    const city = req.params.city
+    console.log('city', city)
     const apiKey = process.env.OPENWEATHER_API_KEY
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
   
